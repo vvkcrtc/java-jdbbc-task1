0d7b41a67fe9bb94709010aca4cb29314fe355f6
Vladimir Komarov <vvkcrtc@gmail.com>
2023-05-01 17:50:26 +0300

    commit 2

--- src/main/java/ru/netology/jdbctask1/JdbcTask1Application.java
+++ src/main/java/ru/netology/jdbctask1/JdbcTask1Application.java
@@ -1,17 +1,8 @@
 package ru.netology.jdbctask1;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-
-import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
 
 @SpringBootApplication
 public class JdbcTask1Application {
